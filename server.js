44b5e9adb2fe8021cbdff056669314915f78284b
Tushar Dangayach <dangayacht@gmail.com>
2025-09-08 12:44:53 +0530

    Updated config

--- server.js
+++ server.js
@@ -71,7 +71,7 @@ app.get("/ui", (_, res) => {
 /**
  * Lifecycle routes
  */
-["save", "publish", "validate", "stop"].forEach((action) => {
+["save", "publish", "validate", "stop", "execute"].forEach((action) => {
   app.post(`/${action}`, (req, res) => {
     log("LIFECYCLE", `${action} called`, req.body);
     res.json({ status: "ok", action });
